65f6921764f808e0849c74c94334ca426dac1772
Eric J. Holmes <eric@ejholmes.net>
2012-03-13 19:38:45 -0700

    Passing specs.

--- jquery.tabs.js
+++ jquery.tabs.js
@@ -1,51 +1,42 @@
-/*
- ** LUM TABS - version 2.3, Authored by Richard Summerhayes for Luminosity Group - www.luminosity-group.com
- 2.3 removed FadeDown, now using relative tab content, removed JS animating height of tab
- 2.2 added obj context so that multiple tabs can go on one page
- 2.1 added IE fallback to display none/block, and Iframe clearing
- */
+;(function($) {
+    /*
+     * Simple tabs plugin for jquery
+     * Version 0.0.1
+     *
+     * Copyright (c) 2012 Luminosity Group
+     */
+    $.fn.tabs = function(options) {
+        return this.each(function() {
+            
+            /* Default settings */
+            var defaults = {
+                /* Selectors */
+                content: '.tabs_content',
+                navigation: '.tabs_nav'
+            };
 
-(function($){
-        $.fn.extend({
-                lumTabs: function(options) {
-                    var defaults = {tabTargetHeight: 400,tabFooterPadding: 40};
-                    var options = $.extend(defaults, options);
+            var settings = $.extend(true, {}, defaults, options);
 
-                    return this.each(function() 
-                    {
-                        var o = options;
-                        var obj = $(this);
-                        var tabTargetHeight = o.tabTargetHeight;
-                        var tabFooterPadding = o.tabFooterPadding;
+            var container            = $(this)
+            var navigation_container = $(settings.navigation, container);
+            var content_container    = $(settings.content, container);
+            var content_items        = $(content_container).children();
 
-                        //When page loads...
-                        $(".tab_content",obj).hide(); //Hide all content
-                        $("ul.tabs > li:first",obj).addClass("active first"); //Activate first tab
-                        $("ul.tabs > li:last",obj).addClass("last"); //Setup last tab
-                        $(".tab_content:first",obj).show().addClass('open'); //Show first tab content
+            $('li a', navigation_container).click(function() {
+                activate_tab($(this).closest('li'));
+            });
 
+            var activate_tab = function(tab) {
+                var id = $('a', tab).attr('href');
 
-                        //On Click Event
-                        $("ul.tabs > li",obj).live('click', function() {
+                $('li', navigation_container).hide();
+                $(tab).show().addClass('active');
 
-                            if(!$(this).hasClass('active')) {
+                $(content_items).hide();
+                $(id, content_container).show().addClass('active');
+            };
 
-                                //TAKE CARE OF TAB of decent Browsers
-                                $("ul.tabs > li",obj).removeClass("active"); //Remove any "active" class
-                                $(this).addClass("active"); //Add "active" class to selected tab
-                                //TAKE CARE OF THE CONTENT
-                                $(".tab_container").css({ height: $(".tab_container").outerHeight(true) });
-                                $(".tab_content",obj).hide().removeClass('open'); //Hide all tab content
-                                $(".tab_content iframe",obj).css('display', 'none');
-                                var activeTab = $(this).find("a").attr("href"); //Find the href attribute value to identify the active tab + content
-                                $(activeTab).find('iframe').css('display', 'block');
-                                $(activeTab).fadeTo('fast', 1).css('zIndex', 100).addClass('open'); //Fade in the active ID content
-                                $(".tab_container").css('height', 'auto' );
-
-                            }//end of active test IF
-                            return false;
-                        });             
-                    });//end of return function
-                }//end of lumTabs
+            activate_tab($('li:first', navigation_container))
         });
+    }
 })(jQuery);
---
+++ spec/javascripts/tabs_spec.js
@@ -0,0 +1,34 @@
+describe('tabs', function() {
+    beforeEach(function() {
+        loadFixtures('tabs.html')
+        $('.tabs').tabs();
+    });
+
+    it('sets the .active class on the first nav item', function() {
+        var item = $('.tabs_nav li:first')
+        expect(item).toHaveClass('active');
+        expect(item).toBeVisible();
+    });
+
+    it('sets the .active class on the first content item', function() {
+        var item = $('.tabs_content > div:first')
+        expect(item).toHaveClass('active');
+        expect(item).toBeVisible();
+    });
+
+    describe('clicking on a tab', function() {
+        beforeEach(function() {
+            $('.tabs_nav li:nth-child(2) a').trigger('click');
+        });
+
+        it('sets the .active class on the nav item that was clicked', function() {
+            var item = $('.tabs_nav li:nth-child(2)');
+            expect(item).toHaveClass('active');
+        });
+
+        it('sets the .active class on the content item for the selected nav item', function() {
+            var item = $('.tabs_content > div:nth-child(2)');
+            expect(item).toHaveClass('active');
+        });
+    });
+});
