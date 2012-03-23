34f6d6130440bf2cc933a7ed510c84630b9a6aa7
Eric J. Holmes <eric@ejholmes.net>
2012-03-22 17:24:29 -0700

    Run the specs both when animation is enabled and when disabled.

--- spec/javascripts/tabs_spec.js
+++ spec/javascripts/tabs_spec.js
@@ -1,54 +1,56 @@
-describe('tabs', function() {
-    beforeEach(function() {
-        loadFixtures('tabs.html')
-        $('.tabs').tabs();
-    });
-    
-    afterEach(function() {
-        window.location.hash = '';
-    });
-
-    it('sets the .active class on the first nav item', function() {
-        var item = $('.tabs_nav li:first')
-        expect(item).toHaveClass('active');
-        expect(item).toBeVisible();
-    });
-
-    it('sets the .active class on the first content item', function() {
-        var item = $('.tabs_content > div:first')
-        expect(item).toHaveClass('active');
-        expect(item).toBeVisible();
-    });
-
-    describe('clicking on a tab', function() {
+[true, false].forEach(function(enabled_animation) {
+    describe('tabs', function() {
         beforeEach(function() {
-            $('.tabs_nav li:nth-child(2)').trigger('click');
+            loadFixtures('tabs.html')
+            $('.tabs').tabs({ animate: enabled_animation });
+        });
+        
+        afterEach(function() {
+            window.location.hash = '';
         });
 
-        it('sets the .active class on the nav item that was clicked', function() {
-            var item = $('.tabs_nav li:nth-child(2)');
+        it('sets the .active class on the first nav item', function() {
+            var item = $('.tabs_nav li:first')
             expect(item).toHaveClass('active');
-            expect($('.tabs_nav li:nth-child(1)')).not.toHaveClass('.active');
+            expect(item).toBeVisible();
         });
 
-        it('sets the .active class on the content item for the selected nav item', function() {
-            var item = $('.tabs_content > div:nth-child(2)');
+        it('sets the .active class on the first content item', function() {
+            var item = $('.tabs_content > div:first')
             expect(item).toHaveClass('active');
-            expect($('.tabs_content > div:nth-child(1)')).not.toHaveClass('active');
+            expect(item).toBeVisible();
         });
 
-        it('adds the hash to window.location.hash', function() {
-            var hash = window.location.hash;
-            expect(hash).toEqual('#tab-tab2');
-        });
-    });
+        describe('clicking on a tab', function() {
+            beforeEach(function() {
+                $('.tabs_nav li:nth-child(2)').trigger('click');
+            });
 
-    describe('page load with a hash', function() {
-        it('sets the .active class on the nav item specified in the url', function() {
-            window.location.hash = '#tab-tab2';
-            $('.tabs').tabs();
-            var item = $('.tabs_nav li:nth-child(2)');
-            expect(item).toHaveClass('active');
+            it('sets the .active class on the nav item that was clicked', function() {
+                var item = $('.tabs_nav li:nth-child(2)');
+                expect(item).toHaveClass('active');
+                expect($('.tabs_nav li:nth-child(1)')).not.toHaveClass('.active');
+            });
+
+            it('sets the .active class on the content item for the selected nav item', function() {
+                var item = $('.tabs_content > div:nth-child(2)');
+                expect(item).toHaveClass('active');
+                expect($('.tabs_content > div:nth-child(1)')).not.toHaveClass('active');
+            });
+
+            it('adds the hash to window.location.hash', function() {
+                var hash = window.location.hash;
+                expect(hash).toEqual('#tab-tab2');
+            });
+        });
+
+        describe('page load with a hash', function() {
+            it('sets the .active class on the nav item specified in the url', function() {
+                window.location.hash = '#tab-tab2';
+                $('.tabs').tabs();
+                var item = $('.tabs_nav li:nth-child(2)');
+                expect(item).toHaveClass('active');
+            });
         });
     });
 });
